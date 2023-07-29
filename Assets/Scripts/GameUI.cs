69cab8de95c96f3ecad4342b357419bcade9ec8a
XA_South\liyuan <liyuan0331@gmail.com>
2023-07-29 11:30:28 +0800

    First commit, there is already a foundation of gameplay.

---
+++ Assets/Scripts/GameUI.cs
@@ -0,0 +1,323 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class GameUI : MonoBehaviour
+{
+    public Texture samplePicture;
+
+    public PicPiece prefabPicPiece;
+    public Material[] baseMaterials;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartGame(4, samplePicture);
+    }
+
+    short curMaxSortCount;
+    int gridCount;
+    PicPiece[,] picPieceArr2;
+    float scale;
+    public void StartGame(int gridCount, Texture picture)
+    {
+        foreach (var mat in baseMaterials)
+        {
+            mat.SetTexture("_Picture", picture);
+        }
+
+        curMaxSortCount = short.MinValue;
+        this.gridCount = gridCount;
+
+        picPieceArr2 = new PicPiece[gridCount, gridCount];
+        scale = 10f / gridCount;
+
+        float dx, dy;
+        dx = dy = scale;
+
+        PicPiece tempPiece;
+        for (int i = 0; i < gridCount; i++)
+        {
+            for (int j = 0; j < gridCount; j++)
+            {
+                tempPiece = GameObject.Instantiate<PicPiece>(prefabPicPiece);
+
+                //创建dirNotch
+                int[] dirNotch = new int[4] { 0, 0, 0, 0 };
+                //上
+                if (j == gridCount - 1) dirNotch[0] = 2;
+                else
+                {
+                    dirNotch[0] = Random.Range(0, 2);
+                }
+                //右
+                if (i == gridCount - 1) dirNotch[1] = 2;
+                else
+                {
+                    dirNotch[1] = Random.Range(0, 2);
+                }
+                //下
+                if (j == 0) dirNotch[2] = 2;
+                else
+                {
+                    dirNotch[2] = picPieceArr2[i, j - 1].dirNotch[0] == 0 ? 1 : 0;
+                }
+                //左
+                if (i == 0) dirNotch[3] = 2;
+                else
+                {
+                    dirNotch[3] = picPieceArr2[i - 1, j].dirNotch[1] == 0 ? 1 : 0;
+                }
+
+                tempPiece.SetUp(gridCount, new Vector2Int(i, j), scale, Indices2Pos(new Vector2Int(i, j)), dirNotch, curMaxSortCount);
+                tempPiece.transform.name = i + "," + j;
+                curMaxSortCount += 2;
+                picPieceArr2[i, j] = tempPiece;
+            }
+        }
+
+        //拼图打散
+        foreach (var forPiece in picPieceArr2)
+        {
+            Vector2 pos;
+            while (true) {
+                float val1 = Random.Range(-8f - scale * .5f, 8f + scale * .5f);
+                float val2 = Random.Range(-8f - scale * .5f, 8f + scale * .5f);
+                
+                if(!(Mathf.Abs(val1)<5.5f+scale * .5f && Mathf.Abs(val2) < 5.5f+scale * .5f))
+                {
+                    pos = new Vector2(val1, val2);
+                    break;
+                }
+            }
+
+            forPiece.transform.position = pos - (Vector2)forPiece.origPos;
+        }
+
+    }
+
+    [HideInInspector]
+    public PicPiece dragingObj = null;
+    Vector3 dragOffset;
+    Ray ray;
+    RaycastHit[] hitArr;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (EventSystem.current.IsPointerOverGameObject())//点击到UI
+            {
+                //Debug.Log("Clicked on the UI");
+                return;
+            }
+
+            dragingObj = null;
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            hitArr = Physics.RaycastAll(ray, 100, 1 << 11);
+            RaycastHit? targetHit = null;
+            foreach (var curHit in hitArr)
+            {
+                if (curHit.transform.GetComponentInParent<PicPiece>().pieceState == PieceState.CornerRight) continue;
+                if (targetHit == null) targetHit = curHit;
+                else
+                {
+                    PicPiece targetPiece = targetHit.Value.transform.GetComponentInParent<PicPiece>();
+                    PicPiece curPiece = curHit.transform.GetComponentInParent<PicPiece>();
+                    if (curPiece.sorting.sortingOrder > targetPiece.sorting.sortingOrder) targetHit = curHit;
+                }
+            }
+            if (targetHit.HasValue)
+            {
+                //print("hit");
+                dragingObj = targetHit.Value.transform.GetComponentInParent<PicPiece>();
+                dragOffset = dragingObj.transform.position - targetHit.Value.point;
+
+                dragingObj.sorting.sortingOrder = curMaxSortCount;
+                curMaxSortCount += 2;
+                //print(dragingObj);
+            }
+        }
+        if (Input.GetMouseButton(0) && dragingObj)
+        {
+            MoveDragingObj();
+        }
+        if (Input.GetMouseButtonUp(0) && dragingObj)
+        {
+            DropPicec();
+            //Vector3 pos = dragingObj.transform.position;
+            //pos.z = 0;
+            //dragingObj.transform.position = pos;
+            //dragingObj = null;
+        }
+    }
+    void MoveDragingObj()
+    {
+        if (!dragingObj) return;
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 100, 1 << 10))
+        {
+            Vector3 targetPos = hit.point + dragOffset - ray.direction * 2.5f;
+            dragingObj.transform.position = targetPos;
+        }
+    }
+
+    void JudgeWin()
+    {
+        //print(FindObjectsOfType<PicPiece>().Length);
+        //if (FindObjectsOfType<PicPiece>().Length <= 4) print("win");
+        int count = 0;
+        foreach (var forPiece in picPieceArr2)
+        {
+            if (forPiece == null) continue;
+            //print(forPiece.name);
+            if (forPiece.pieceState != PieceState.CornerRight) count++;
+        }
+        //print(count);
+        if (count == 0) print("win!");
+    }
+
+    const float fitPercent = .15f;
+    void DropPicec()
+    {
+        if (!dragingObj) return;
+        Vector3 pos = dragingObj.transform.position;
+        pos.z = 0;
+
+        if (dragingObj.pieceState == PieceState.Corner
+            && Vector2.Distance(dragingObj.transform.position, Vector2.zero) < scale * fitPercent)
+        {
+            pos = Vector2.zero;
+            dragingObj.transform.position = pos;
+            dragingObj.SetCornerRight();
+        }
+        else
+        {
+            foreach (var forPiece in picPieceArr2)
+            {
+                if (forPiece == null || forPiece == dragingObj) continue;
+
+                if (Vector2.Distance(dragingObj.transform.position, forPiece.transform.position) < scale * fitPercent)
+                {
+                    //pos = Vector2.zero;
+                    for (int i = 0; i < dragingObj.transform.childCount; i++)
+                    {
+                        for (int j = 0; j < forPiece.transform.childCount; j++)
+                        {
+                            Transform transformI = dragingObj.transform.GetChild(i);
+                            Transform transformJ = forPiece.transform.GetChild(j);
+                            if (Vector2.Distance(transformI.position, transformJ.position) < scale * (1 + fitPercent))
+                            {
+                                pos = forPiece.transform.position;
+                                dragingObj.transform.position = pos;
+                                forPiece.CombinePiece(dragingObj);
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+
+        //dragingObj.transform.position = pos;
+        JudgeWin();
+        dragingObj = null;
+    }
+
+    //PicPiece GetPieceWithIndices(Vector2Int indices)
+    //{
+    //    foreach(var curPiece in picPieceArr2)
+    //    {
+    //        if (curPiece == null) continue;
+    //        foreach (var curIndices in curPiece.containIndicesArr)
+    //        {
+    //            if (curIndices == indices) return curPiece;
+    //        }
+    //    }
+
+    //    return null;
+    //}
+
+    Vector2 Indices2Pos(Vector2Int indices)
+    {
+        float dx, dy;
+        dx = dy = 10f / gridCount;
+
+        Vector2 result = new Vector2(dx * (indices.x + .5f), dy * (indices.y + .5f)) - Vector2.one * 5f;
+        //result.x /= -1f;
+        return new Vector2(dx * (indices.x + .5f), dy * (indices.y + .5f)) - Vector2.one * 5f;
+    }
+
+    //Vector2Int[] GetAllNearIndices(PicPiece picPiece)
+    //{
+    //    List<Vector2Int> result = new List<Vector2Int>();
+
+    //    foreach(var indices in picPiece.containIndicesArr)
+    //    {
+    //        Vector2Int newIndices;
+    //        if (indices.x > 0)
+    //        {
+    //            newIndices = indices + Vector2Int.left;
+    //            //print(1);
+    //            if (!picPiece.containIndicesArr.Contains(newIndices))
+    //                result.Add(newIndices);
+    //        }
+    //        if (indices.x < gridCount - 1)
+    //        {
+    //            newIndices = indices + Vector2Int.right;
+    //            //print(2);
+    //            if (!picPiece.containIndicesArr.Contains(newIndices))
+    //                result.Add(newIndices);
+    //        }
+    //        if (indices.y > 0)
+    //        {
+    //            newIndices = indices + Vector2Int.down;
+    //            //print(3);
+    //            if (!picPiece.containIndicesArr.Contains(newIndices))
+    //                result.Add(newIndices);
+    //        }
+    //        if(indices.y < gridCount - 1)
+    //        {
+    //            newIndices = indices + Vector2Int.up;
+    //            //print(4);
+    //            if (!picPiece.containIndicesArr.Contains(newIndices))
+    //                result.Add(newIndices);
+    //        }
+    //    }
+
+    //    return result.ToArray();
+    //}
+
+    public struct Range
+    {
+        public float min;
+        public float max;
+        public float range { get { return max - min + 1; } }
+        public Range(float aMin, float aMax)
+        {
+            min = aMin; max = aMax;
+        }
+    }
+
+    public static float RandomValueFromRanges(params Range[] ranges)
+    {
+        if (ranges.Length == 0)
+            return 0;
+        float count = 0;
+        foreach (Range r in ranges)
+            count += r.range;
+        float sel = Random.Range(0, count);
+        foreach (Range r in ranges)
+        {
+            if (sel < r.range)
+            {
+                return r.min + sel;
+            }
+            sel -= r.range;
+        }
+        return 0;
+    }
+}
---
+++ Assets/Scripts/PicPiece.cs
@@ -0,0 +1,178 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public enum PieceState { Normal,Corner,CornerRight}
+public class PicPiece : MonoBehaviour
+{
+    public Transform[] pieceArr;//0,1,2分别是凹、凸、平面
+    public Texture[] lightMap0Arr;
+    public Texture[] lightMap1Arr;
+    public Texture[] lightMap2Arr;
+
+    [HideInInspector]
+    public int[] dirNotch;
+
+    [HideInInspector]
+    public Vector3 origPos;
+
+    public List<Vector2Int> containIndicesArr;
+    public Vector2Int mainIndices;
+
+    public PieceState pieceState = PieceState.Normal;
+
+    [HideInInspector]
+    public SortingGroup sorting;
+    //Start is called before the first frame update
+    void Awake()
+    {
+        containIndicesArr = new List<Vector2Int>();
+        sorting = GetComponent<SortingGroup>();
+        //SetUp(3, Vector3.zero, new int[] { 0, 1, 2, 2 });
+    }
+
+    public void SetCornerRight()
+    {
+        if (pieceState != PieceState.Corner) return;
+        sorting.sortingOrder = short.MinValue;
+        pieceState = PieceState.CornerRight;
+    }
+
+    public void CombinePiece(PicPiece targetPiece)
+    {
+        if (targetPiece == null || targetPiece == this) return;
+
+        if (targetPiece.pieceState == PieceState.Normal)
+        {
+            CombinePieceNojudge(targetPiece);
+        }
+        else
+        {
+            targetPiece.CombinePieceNojudge(this);
+        }
+    }
+    public void CombinePieceNojudge(PicPiece targetPiece)
+    {
+        //print("合并");
+        for (int i = targetPiece.transform.childCount - 1; i >= 0; i--) 
+        {
+            targetPiece.transform.GetChild(i).parent = transform;
+        }
+
+        targetPiece.pieceState = PieceState.CornerRight;
+        GameObject.Destroy(targetPiece.gameObject);
+    }
+
+    public void SetUp(int gridCount, Vector2Int indices, float scale, Vector2 origPos, int[] dirNotch, int sortCount)//dirNotch传递长度为4的数组，分别表示上、右、下、左
+    {
+        if (dirNotch.Length != 4) {
+            Debug.LogWarning("必须传递长度为4的数组，分别表示上、右、下、左");
+            return;
+        }
+        
+        this.origPos = origPos;
+        this.dirNotch = dirNotch;
+        sorting.sortingOrder = sortCount;
+        mainIndices = indices;
+        containIndicesArr.Add(indices);
+        if (containIndicesArr.Contains(new Vector2Int(0, 0))
+            || containIndicesArr.Contains(new Vector2Int(gridCount - 1, 0))
+            || containIndicesArr.Contains(new Vector2Int(0, gridCount - 1))
+            || containIndicesArr.Contains(new Vector2Int(gridCount - 1, gridCount - 1))
+            )
+        {
+            pieceState = PieceState.Corner;
+        }
+
+        transform.position = Vector3.zero;
+        Transform childTrans = transform.GetChild(0);
+
+        childTrans.localScale = Vector3.one * scale;
+        childTrans.position = origPos;
+        childTrans.name = "" + indices.ToString();
+
+        int zAngle = 0;
+        Transform tempTrans;
+        for (int i = 0; i < 4; i++)
+        {
+            tempTrans = GameObject.Instantiate<Transform>(pieceArr[dirNotch[i]], childTrans);
+            tempTrans.localPosition = Vector3.zero;
+            tempTrans.rotation = Quaternion.Euler(0, 180, zAngle);
+
+            //Light贴图与反转
+            Material tempMat = tempTrans.GetComponent<MeshRenderer>().material;
+            switch (dirNotch[i])
+            {
+                case 0://凹下
+                    if (zAngle == 90 || zAngle == 180)
+                    {
+                        tempMat.SetTexture("_AddTex", lightMap0Arr[1]);
+                    }
+                    if (zAngle == 180 || zAngle == 270)
+                    {
+                        tempMat.SetInt("_LightFlip", 1);
+                    }
+                    break;
+                case 1://突起
+                    if (zAngle == 90 || zAngle == 180)
+                    {
+                        tempMat.SetTexture("_AddTex", lightMap1Arr[1]);
+                    }
+                    if (zAngle == 180 || zAngle == 270)
+                    {
+                        tempMat.SetInt("_LightFlip", 1);
+                    }
+                    break;
+                case 2://平面
+                    if (zAngle == 90 || zAngle == 180)
+                    {
+                        tempMat.SetTexture("_AddTex", lightMap2Arr[1]);
+                    }
+                    if (zAngle == 180 || zAngle == 270)
+                    {
+                        tempMat.SetInt("_LightFlip", 1);
+                    }
+                    break;
+            }
+
+
+            //设置UV2
+            Mesh mesh = tempTrans.GetComponent<MeshFilter>().mesh;
+            Vector3[] vertices = mesh.vertices;
+            Vector2[] uvs = new Vector2[vertices.Length];
+            //Matrix4x4 localToWorld = tempTrans.localToWorldMatrix;
+            for (int j = 0; j < uvs.Length; j++)
+            {
+                Vector3 worldVertice = tempTrans.TransformPoint(vertices[j]);
+                //Vector3 worldVertice = localToWorld.MultiplyPoint3x4(vertices[j]);
+                //print("~~~~~~");
+                //print(worldVertice);
+                //print(vertices[j]);
+                float remapU = worldVertice.x.Remap(-5, 5, 0, 1);
+                float remapV = worldVertice.y.Remap(-5, 5, 0, 1);
+                uvs[j] = new Vector2(remapU, remapV);
+            }
+            mesh.uv2 = uvs;
+
+            //方向旋转
+            zAngle += 90;
+        }
+    }
+}
+
+//public struct PieceInfo
+//{
+//    public Vector2Int indices;
+//    public Vector2 position;
+//}
+
+public static class ExtensionMethods
+{
+
+    public static float Remap(this float value, float from1, float to1, float from2, float to2)
+    {
+        return (value - from1) / (to1 - from1) * (to2 - from2) + from2;
+    }
+
+}
